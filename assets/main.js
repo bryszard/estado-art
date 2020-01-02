33eec6e5eae9ab2b11ded456e82e77f4c6a866bd
Piotr Brych <pbrych@gmail.com>
2020-01-03 00:53:52 +0100

    Add simple gallery

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,8 @@
   var hashLinks = document.querySelectorAll('a[href^="#"]');
   for (var i = 0; i < hashLinks.length; i++) {
     var hashLink = hashLinks[i];
-    var targetSelector = hashLink.getAttribute('href');
-    hashLink.addEventListener('click', function(event) {
+    var targetSelector = hashLink.getAttribute("href");
+    hashLink.addEventListener("click", function(event) {
       event.preventDefault();
       Jump(targetSelector, {
         duration: 1000
